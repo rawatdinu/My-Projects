df30ab9929a8d1eff37349143933883bb042a1f2
dinesh-PC\dinesh <rawatdinu@gmail.com>
2018-10-20 20:17:58 +0530

    Don't user MDI, User simple form instead. Demo done

---
+++ Dinesh Demo Project/POC/MDIForm/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace MDIForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.cmdItemMaster = new System.Windows.Forms.Button();
+            this.ddPanel = new System.Windows.Forms.Panel();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.cmdMaster = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.cmdClose = new System.Windows.Forms.Button();
+            this.cmdTrans = new System.Windows.Forms.Button();
+            this.pnlContainer = new System.Windows.Forms.Panel();
+            this.ddPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(39)))), ((int)(((byte)(160)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(1090, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 595);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(39)))), ((int)(((byte)(160)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 595);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(39)))), ((int)(((byte)(160)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(10, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1080, 10);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(39)))), ((int)(((byte)(160)))));
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(10, 585);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1080, 10);
+            this.panel4.TabIndex = 3;
+            // 
+            // cmdItemMaster
+            // 
+            this.cmdItemMaster.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
+            this.cmdItemMaster.FlatAppearance.BorderSize = 0;
+            this.cmdItemMaster.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmdItemMaster.ForeColor = System.Drawing.Color.White;
+            this.cmdItemMaster.Location = new System.Drawing.Point(231, 17);
+            this.cmdItemMaster.Name = "cmdItemMaster";
+            this.cmdItemMaster.Size = new System.Drawing.Size(169, 46);
+            this.cmdItemMaster.TabIndex = 7;
+            this.cmdItemMaster.Text = "Item";
+            this.cmdItemMaster.UseVisualStyleBackColor = false;
+            this.cmdItemMaster.Click += new System.EventHandler(this.cmdItemMaster_Click);
+            // 
+            // ddPanel
+            // 
+            this.ddPanel.Controls.Add(this.button11);
+            this.ddPanel.Controls.Add(this.button10);
+            this.ddPanel.Controls.Add(this.button9);
+            this.ddPanel.Controls.Add(this.button8);
+            this.ddPanel.Controls.Add(this.button7);
+            this.ddPanel.Controls.Add(this.button6);
+            this.ddPanel.Controls.Add(this.cmdMaster);
+            this.ddPanel.Location = new System.Drawing.Point(16, 16);
+            this.ddPanel.MaximumSize = new System.Drawing.Size(196, 259);
+            this.ddPanel.MinimumSize = new System.Drawing.Size(196, 45);
+            this.ddPanel.Name = "ddPanel";
+            this.ddPanel.Size = new System.Drawing.Size(196, 47);
+            this.ddPanel.TabIndex = 8;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(87)))), ((int)(((byte)(194)))));
+            this.button11.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button11.FlatAppearance.BorderSize = 0;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.Color.White;
+            this.button11.Location = new System.Drawing.Point(0, 221);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(196, 35);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "City";
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(87)))), ((int)(((byte)(194)))));
+            this.button10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button10.FlatAppearance.BorderSize = 0;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.Color.White;
+            this.button10.Location = new System.Drawing.Point(0, 186);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(196, 35);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "State Master";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(87)))), ((int)(((byte)(194)))));
+            this.button9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.White;
+            this.button9.Location = new System.Drawing.Point(0, 151);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(196, 35);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Rate Master";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(87)))), ((int)(((byte)(194)))));
+            this.button8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(0, 116);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(196, 35);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Company Master";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(87)))), ((int)(((byte)(194)))));
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(0, 81);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(196, 35);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Item Master";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(87)))), ((int)(((byte)(194)))));
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(0, 46);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(196, 35);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Party Master";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // cmdMaster
+            // 
+            this.cmdMaster.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
+            this.cmdMaster.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cmdMaster.FlatAppearance.BorderSize = 0;
+            this.cmdMaster.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmdMaster.ForeColor = System.Drawing.Color.White;
+            this.cmdMaster.Image = global::MDIForm.Properties.Resources.down;
+            this.cmdMaster.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cmdMaster.Location = new System.Drawing.Point(0, 0);
+            this.cmdMaster.Name = "cmdMaster";
+            this.cmdMaster.Size = new System.Drawing.Size(196, 46);
+            this.cmdMaster.TabIndex = 6;
+            this.cmdMaster.Text = "Master";
+            this.cmdMaster.UseVisualStyleBackColor = false;
+            this.cmdMaster.Click += new System.EventHandler(this.cmdMaster_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 3;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // cmdClose
+            // 
+            this.cmdClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
+            this.cmdClose.FlatAppearance.BorderSize = 0;
+            this.cmdClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmdClose.ForeColor = System.Drawing.Color.White;
+            this.cmdClose.Location = new System.Drawing.Point(1046, 12);
+            this.cmdClose.Name = "cmdClose";
+            this.cmdClose.Size = new System.Drawing.Size(38, 33);
+            this.cmdClose.TabIndex = 9;
+            this.cmdClose.Text = "X";
+            this.cmdClose.UseVisualStyleBackColor = false;
+            this.cmdClose.Click += new System.EventHandler(this.cmdClose_Click);
+            // 
+            // cmdTrans
+            // 
+            this.cmdTrans.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
+            this.cmdTrans.FlatAppearance.BorderSize = 0;
+            this.cmdTrans.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmdTrans.ForeColor = System.Drawing.Color.White;
+            this.cmdTrans.Location = new System.Drawing.Point(417, 17);
+            this.cmdTrans.Name = "cmdTrans";
+            this.cmdTrans.Size = new System.Drawing.Size(169, 46);
+            this.cmdTrans.TabIndex = 10;
+            this.cmdTrans.Text = "Transation";
+            this.cmdTrans.UseVisualStyleBackColor = false;
+            // 
+            // pnlContainer
+            // 
+            this.pnlContainer.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlContainer.Location = new System.Drawing.Point(10, 69);
+            this.pnlContainer.Name = "pnlContainer";
+            this.pnlContainer.Size = new System.Drawing.Size(1080, 516);
+            this.pnlContainer.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1100, 595);
+            this.Controls.Add(this.pnlContainer);
+            this.Controls.Add(this.cmdTrans);
+            this.Controls.Add(this.cmdClose);
+            this.Controls.Add(this.ddPanel);
+            this.Controls.Add(this.cmdItemMaster);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ddPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button cmdMaster;
+        private System.Windows.Forms.Button cmdItemMaster;
+        private System.Windows.Forms.Panel ddPanel;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button cmdClose;
+        private System.Windows.Forms.Button cmdTrans;
+        private System.Windows.Forms.Panel pnlContainer;
+    }
+}
+
---
+++ Dinesh Demo Project/POC/MDIForm/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MDIForm
+{
+    public partial class Form1 : Form
+    {
+        private bool iscollapsed = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (iscollapsed)
+            {
+                cmdMaster.Image = MDIForm.Properties.Resources.down;
+                ddPanel.Height += 10;
+                if (ddPanel.Height == ddPanel.MaximumSize.Height)
+                {
+                    timer2.Stop();
+                    iscollapsed = false;
+                }
+            }
+            else
+            {
+                cmdMaster.Image = MDIForm.Properties.Resources.up;
+                ddPanel.Height -= 10;
+                if (ddPanel.Height == ddPanel.MinimumSize.Height)
+                {
+                    timer2.Stop();
+                    iscollapsed = true;
+                }
+            }
+
+        }
+
+        private void cmdMaster_Click(object sender, EventArgs e)
+        {
+
+            timer2.Start();
+        }
+
+        private void cmdClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cmdItemMaster_Click(object sender, EventArgs e)
+        {
+            //ItemMaster obj = new ItemMaster();
+            //this.pnlContainer.Controls.Add(obj);
+
+            //obj.Show();
+
+            ItemMaster objForm = new ItemMaster();
+            objForm.TopLevel = false;
+            this.pnlContainer.Controls.Add(objForm);
+            objForm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            //objForm.Dock = DockStyle.Fill;
+            objForm.Show();
+
+
+        }
+    }
+}
